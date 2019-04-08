1c0ec7479711faeb25e029a4cd8c17ccfdaad3d2
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2019-04-07 21:44:24 -0400

    Trying to fix path

--- ci/build.sh
+++ ci/build.sh
@@ -7,10 +7,10 @@ export PROJECT_NAME=KlSS
 export BUILD_CONFIGURATION='release'
 
 export ARTIFACT_FILENAME=${PROJECT_NAME}-${BUILD_PLATFORM}.zip
-export ARTIFACTS_DIR=~/repo/artifacts
+export ARTIFACTS_DIR=/root/repo/artifacts
 export ARTIFACT_FULL_PATH=${ARTIFACTS_DIR}/${OUTPUT_FILE}
 
-export PUBLISH_DIR=~/repo/bin/release/netcoreapp2.2/${BUILD_PLATFORM}/publish
+export PUBLISH_DIR=/root/repo/bin/release/netcoreapp2.2/${BUILD_PLATFORM}/publish
 export MGCB_VERSION=3.7.0.4
 export MGCB_PACKAGE_PATH=/root/.nuget/packages/monogame.content.builder/${MGCB_VERSION}/build/MGCB/build
 
